860835673b84f3b452129be5eca54df963ecc3d6
rakey <rakeya@yandex.ru>
2024-03-05 22:56:58 +0300

    HW lesson2 -done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,16 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        Configuration.pageLoadStrategy= "eager" ;
+        open("https://www.google.com/");
+        $(byName("q")).setValue("selenide").pressEnter();
+        $("#search").shouldHave(text("https://selenide.org"));
+    }
+}
